e18d1f06c0d13986079b6b1f36e82feddd326161
Toni  Diep <adiep123@gmail.com>
2020-05-06 07:05:59 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -12,14 +12,12 @@ require 'pry'
 #   end
 # end
 
-# def my_find(collection)
-#   i = 0
-#   while i < collection.length
-#     if yield(collection[i])
-#       return collection[i]
-#     end
-#     i = i + 1
-#   end
-# end
-
-my_find(collection) {|1| 1 % 3 == 0 and 1 % 5 == 0 }
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
+end
